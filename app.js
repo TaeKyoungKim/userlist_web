e7984b5ae667f8af046b5c305066b12fa9acc35f
GaryKim <moduedu@gmail.com>
2020-06-17 17:50:11 +0900

    mysql insert form

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express  = require('express')
+var path  = require('path')
+var app  = express() 
+var apiRouter = require('./routes/api_router')
 
-var app  = express()
 
-var port = 8000;
-app.listen(port, function(){
-    console.log(`Server is Runing at http://localhost:8080`)
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.use('/', apiRouter)
+
+// console.log(path.join(__dirname , '/views')) 
+var port = 8000;  
+app.listen(port, ()=>{
+    console.log(`Server is Runing at http://localhost:${port}`)
 })
